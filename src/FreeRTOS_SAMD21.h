8557db83d2bc50275c83f330e5af843097e7046e
Steven Johnson <sakurainds@gmail.com>
2018-05-20 14:10:41 +0800

    Not system includes, local includes

--- src/FreeRTOS_SAMD21.h
+++ src/FreeRTOS_SAMD21.h
@@ -5,22 +5,22 @@
 
 	#include <Arduino.h> //required to prevent a compiling error when a cpp header file tries to include this library
 
-	#include <FreeRTOS.h>
-	#include <timers.h>
-	#include <task.h>
-	#include <StackMacros.h>
-	#include <semphr.h>
-	#include <queue.h>
-	#include <projdefs.h>
-	#include <portmacro.h>
-	#include <portable.h>
-	#include <mpu_wrappers.h>
-	#include <list.h>
-	#include <FreeRTOSConfig.h>
-	#include <event_groups.h>
-	#include <error_hooks.h>
-	#include <deprecated_definitions.h>
-	#include <croutine.h>
+	#include "FreeRTOS.h"
+	#include "timers.h"
+	#include "task.h"
+	#include "StackMacros.h"
+	#include "semphr.h"
+	#include "queue.h"
+	#include "projdefs.h"
+	#include "portmacro.h"
+	#include "portable.h"
+	#include "mpu_wrappers.h"
+	#include "list.h"
+	#include "FreeRTOSConfig.h"
+	#include "event_groups.h"
+	#include "error_hooks.h"
+	#include "deprecated_definitions.h"
+	#include "croutine.h"
 
 
 #endif
